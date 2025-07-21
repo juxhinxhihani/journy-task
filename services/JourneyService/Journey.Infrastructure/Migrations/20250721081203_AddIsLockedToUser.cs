bfac84d876ee9aa0fd912f3a3fdb39fad2df67d1
Juxhin Xhihani <juxhinxhihani@gmail.com>
2025-07-21 15:09:38 +0200

    add migrations

---
+++ services/JourneyService/Journey.Infrastructure/Migrations/20250721081203_AddIsLockedToUser.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Journey.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsLockedToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RouteDistanceKm",
+                table: "Journeys",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Journeys",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LoginRetry",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Journeys");
+
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LoginRetry",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RouteDistanceKm",
+                table: "Journeys",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
